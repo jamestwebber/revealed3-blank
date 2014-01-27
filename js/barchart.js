956e445bd0b0c9869eb3bc775a422c76ec328b56
James Webber <jamestwebber@gmail.com>
2014-01-27 10:27:26 -0800

    Adding generic barchart and scatter plots

---
+++ js/barchart.js
@@ -0,0 +1,154 @@
+/*
+ * Function to create scatter plots in a consistent manner
+ * adapted from Mike Bostock: http://bost.ocks.org/mike/chart/
+ */
+
+function barchart(selection) {
+  var margin = { top: 40, right: 40, bottom: 80, left: 60 },
+      width = 700, // default width
+      height = 700; // default height
+      xValue = function(d) { return d[0]; }, // x is categorical
+      yValue = function(d) { return +d[1]; },
+      xScale = d3.scale.ordinal(),
+      yScale = d3.scale.linear(),
+      xAxis = d3.svg.axis().scale(xScale).orient("bottom"),
+      yAxis = d3.svg.axis().scale(yScale).orient("left"),
+      yLabel = "";
+
+  function chart(selection) {
+    selection.each(function(data) {
+
+      // Convert data to standard representation greedily;
+      // this is needed for nondeterministic accessors.
+      data = data.map(function(d, i) {
+        return [xValue.call(data, d, i),
+                yValue.call(data, d, i)];
+      });
+
+      // Update the x-scale.
+      xScale.domain(data.map(function (d) { return d[0]; }))
+        .rangeRoundBands([0, width - margin.left - margin.right], 0.4);
+
+      // Update the y-scale.
+      yScale.domain([0, 1])
+        .range([height - margin.top - margin.bottom, 0])
+        .clamp(true);
+
+      // Select the svg element, if it exists.
+      var svg = d3.select(this).selectAll("svg").data([data]);
+
+      // Otherwise, create the skeletal chart.
+      var gEnter = svg.enter().append("svg")
+        .attr("class", "d3")
+        .append("g");
+
+      // Add axes and axes labels
+      gEnter.append("g").attr("class", "d3 x axis")
+
+      gEnter.append("g").attr("class", "d3 y axis")
+        .append("text")
+          .attr("class", "label")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text(yLabel);
+
+      // Update the outer dimensions.
+      svg.attr("width", width)
+        .attr("height", height);
+
+      // Update the inner dimensions.
+      var g = svg.select("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // Update the bar chart
+      var bars = g.selectAll(".bar")
+        .data(data, function(d) { return d[0]; });
+
+      // transition existing points
+      bars.transition()
+        .delay(function(d, i) { return i * 25; })
+        .duration(data.length * 25)
+        .attr("y", function(d) { return yScale(d[1]); })
+        .attr("height", function(d) {
+          return height - margin.top - margin.bottom - yScale(d[1]);
+        });
+
+      // create any new ones
+      bars.enter()
+        .append("rect")
+          .attr("class", "d3 bar")
+          .attr("x", function(d) { return xScale(d[0]); })
+          .attr("width", xScale.rangeBand())
+          .attr("y", height - margin.top - margin.bottom)
+          .attr("height", 0)
+        .transition()
+          .delay(function(d, i) { return i * 25; })
+          .duration(data.length * 25)
+          .attr("y", function(d) { return yScale(d[1]); })
+          .attr("height", function(d) {
+            return height - margin.top - margin.bottom - yScale(d[1]);
+          });
+
+      // remove any old ones
+      bars.exit()
+        .transition()
+          .attr("y", height - margin.top - margin.bottom)
+          .attr("height", 0)
+          .remove();
+
+      g.select("g.x.axis")
+        .attr("transform", "translate(0," + yScale.range()[0] + ")")
+        .call(xAxis)
+        .selectAll("text")
+          .style("font-size", "80%")
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", "rotate(-65)");
+
+      // Update the y-axis.
+      g.select("g.y.axis")
+        .call(yAxis);
+    });
+  }
+
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
+
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  chart.x = function(_) {
+    if (!arguments.length) return xValue;
+    xValue = _;
+    return chart;
+  };
+
+  chart.y = function(_) {
+    if (!arguments.length) return yValue;
+    yValue = _;
+    return chart;
+  };
+
+  chart.yLabel = function(_) {
+    if (!arguments.length) return yLabel;
+    yLabel = _;
+    return chart;
+  };
+
+  return chart;
+}
---
+++ js/scatter.js
@@ -0,0 +1,175 @@
+/*
+ * Function to create scatter plots in a consistent manner
+ * adapted from Mike Bostock: http://bost.ocks.org/mike/chart/
+ */
+
+function scatter(selection) {
+  var margin = { top: 40, right: 40, bottom: 50, left: 60 },
+      width = 700, // default width
+      height = 700; // default height
+      nameFn = function(d) { return d.name; }, // name function
+      xValue = function(d) { return +d[0]; },
+      yValue = function(d) { return +d[1]; },
+      rValue = function(d) { return 3.5; }, // default radius
+      xScale = d3.scale.linear(),
+      yScale = d3.scale.linear(),
+      xAxis = d3.svg.axis().scale(xScale).orient("bottom"),
+      yAxis = d3.svg.axis().scale(yScale).orient("left"),
+      xLabel = "", // default is no labels
+      yLabel = "";
+
+  function chart(selection) {
+    selection.each(function(data) {
+
+      // Convert data to standard representation greedily;
+      // this is needed for nondeterministic accessors.
+      data = data.map(function(d, i) {
+        return [xValue.call(data, d, i),
+                yValue.call(data, d, i),
+                rValue.call(data, d, i),
+                nameFn.call(data, d, i)];
+      });
+
+      // Update the x-scale.
+      xScale.domain(d3.extent(data, function(d) { return d[0]; }))
+        .range([0, width - margin.left - margin.right])
+        .nice();
+
+      // Update the y-scale.
+      yScale.domain(d3.extent(data, function(d) { return d[1]; }))
+        .range([height - margin.top - margin.bottom, 0])
+        .nice();
+
+      // Select the svg element, if it exists.
+      var svg = d3.select(this).selectAll("svg").data([data]);
+
+      // Otherwise, create the skeletal chart.
+      var gEnter = svg.enter().append("svg").append("g");
+
+      // Add axes and axes labels
+      gEnter.append("g").attr("class", "d3 x axis")
+        .append("text")
+          .attr("class", "label")
+          .attr("x", width - margin.left - margin.right)
+          .attr("y", -6)
+          .style("text-anchor", "end")
+          .text(xLabel);
+
+      gEnter.append("g").attr("class", "d3 y axis")
+        .append("text")
+          .attr("class", "label")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text(yLabel);
+
+      // Update the outer dimensions.
+      svg.attr("width", width)
+        .attr("height", height);
+
+      // Update the inner dimensions.
+      var g = svg.select("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // Update the scatter plot
+      var circles = g.selectAll(".dot")
+        .data(data, function(d) { return d[3]; });
+
+      // transition existing points
+      circles.transition()
+        .attr("r", function(d) { return d[2]; })
+        .transition()
+          .delay(function(d, i) { return 250 + i * 10; })
+          .duration(250 + data.length * 10)
+          .attr("cx", function(d) { return xScale(d[0]); })
+          .attr("cy", function(d) { return yScale(d[1]); });
+
+      // create any new ones
+      circles.enter()
+        .append("circle")
+          .attr("class", "d3 dot")
+          .attr("r", 0)
+          .attr("cx", function(d) { return xScale(d[0]); })
+          .attr("cy", function(d) { return yScale(d[1]); })
+        .transition()
+          .duration(750)
+          .attr("r", function(d) {return d[2]; });
+
+      // remove any old ones
+      circles.exit()
+        .transition()
+          .attr("r", 0)
+          .remove()
+
+      // Update the x-axis.
+      g.select(".d3.x.axis")
+        .attr("transform", "translate(0," + yScale.range()[0] + ")")
+        .call(xAxis)
+        .select("text")
+        .text(xLabel);
+
+      // Update the y-axis.
+      g.select(".d3.y.axis")
+        .call(yAxis)
+        .select("text")
+        .text(yLabel);
+    });
+  }
+
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
+
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  chart.id = function(_) {
+    if (!arguments.length) return nameFn;
+    nameFn = _;
+    return chart;
+  };
+
+  chart.x = function(_) {
+    if (!arguments.length) return xValue;
+    xValue = _;
+    return chart;
+  };
+
+  chart.y = function(_) {
+    if (!arguments.length) return yValue;
+    yValue = _;
+    return chart;
+  };
+
+  chart.r = function(_) {
+    if (!arguments.length) return rValue;
+    rValue = _;
+    return chart
+  }
+
+  chart.xLabel = function(_) {
+    if (!arguments.length) return xLabel;
+    xLabel = _;
+    return chart;
+  };
+
+  chart.yLabel = function(_) {
+    if (!arguments.length) return yLabel;
+    yLabel = _;
+    return chart;
+  };
+
+  return chart;
+}
